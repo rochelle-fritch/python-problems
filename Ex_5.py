17bbecc76e03851b20e760a599b6a551c260c0d1
rochelle-fritch <36424255+rochelle-fritch@users.noreply.github.com>
2018-04-08 18:23:22 +0100

    Update Ex_5.py

--- Ex_5.py
+++ Ex_5.py
@@ -2,6 +2,7 @@
 # Rochelle Fritch
 # Exercise 5, 04/03/2018
 
+# Notes
 # Opening data files in python, printing formatted data in python.
 
 # Different ways to open files:
@@ -11,9 +12,9 @@
 #   r+ = read + write
 
 # with - automatically closes a file after.
-# Look @ Section 7.1 Fancier output
+# Look @ Section 7.1 Fancier output, Python tutorial
 
-# 
+# Exercise 5
 # Write a Python script that reads the Iris data set in and prints the four numerical values on each row in a nice format. 
 # That is, on the screen should be printed the petal length, petal width, sepal length and sepal width, and these values 
 # should have the decimal places aligned, with a space between the columns.
@@ -24,6 +25,3 @@ with open("data/iris.csv") as f:
         x = line.split(',')         # splits lines into a list
         print(x[0],x[1],x[2],x[3])  # formats with correct blocking
    
-
-
-
