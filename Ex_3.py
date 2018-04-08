3ea8fc3f778f24bc88964cc932c1702d1a3dee69
rochelle-fritch <36424255+rochelle-fritch@users.noreply.github.com>
2018-04-08 17:19:50 +0100

    Create Ex_3.py

---
+++ Ex_3.py
@@ -0,0 +1,16 @@
+# Rochelle Fritch 11.02.2018
+
+def collatz(x):
+
+    if x % 2 == 0:
+        print(x // 2)
+        return x // 2
+
+    elif x % 2 == 1:
+        r = 3 * x + 1
+        print(r)
+        return r
+
+n = input("Pick a number: ")
+while n != 1:
+    n = collatz(int(n))
